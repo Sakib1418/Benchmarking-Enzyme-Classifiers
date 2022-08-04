30476b4557a1b9ef5777f3431022439ad8746150
Sakib1418 <66162359+Sakib1418@users.noreply.github.com>
2022-08-04 11:38:42 -0500

    Update ECPred.sh
    Edited ECpred

--- ECPred.sh
+++ ECPred.sh
@@ -1,41 +1,53 @@
 #/usr/bin/bash
-
+rm -r files
+echo files deleted
 mkdir files
 
-mv $1 files/
+cp $1 files/
 
-cd {path to ECpred folder}/files
+cd /home/reuel/Desktop/Sakib/ML/classification/ECpred/ECPred/files
 
-split -l 10 $1 l
+split -l 2 $1 l
+echo list of files are 
+ls
+
+rm $1
+echo list of files after deleting main file
 
 for fil in *;do
-
+echo $fil
 mv $fil $fil.fasta
 done
 
 wait
 
-for file in *.fasta;do
-sleep 3
-mv $file {path to ECpred folder}
-cd {path to ECpred folder}
-java -jar ECPred.jar weighted $file {path to ECpred folder} temp/ ${file}_results.tsv &
-sleep 3
-cd {path to ECpred folder}/files
+for file in *.fasta;
+do
+sleep .1
+mv $file /home/reuel/Desktop/Sakib/ML/classification/ECpred/ECPred/
+cd /home/reuel/Desktop/Sakib/ML/classification/ECpred/ECPred
+java -jar ECPred.jar weighted $file /home/reuel/Desktop/Sakib/ML/classification/ECpred/ECPred/ temp/ ${file}_results.tsv &
+sleep .25
+cd /home/reuel/Desktop/Sakib/ML/classification/ECpred/ECPred/files
 done
 
 wait
 
 echo Prediction completed
-sleep 2
-
-cd {path to ECpred folder}
-
-cat *.tsv >> sample_result.txt
 
 
+cd /home/reuel/Desktop/Sakib/ML/classification/ECpred/ECPred/
 
+ls *.tsv
+rm sample_result.txt
+
+for res in *.tsv;
+do
+cat $res | tail -1 >> sample_result.txt
+done
+
+wait 
+
+rm *.fasta
+rm *.tsv
 echo DONE
-
-
-
