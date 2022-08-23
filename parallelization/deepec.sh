b803bc07b1862359006efbce8334afd0386a8ee0
Sakib1418 <66162359+Sakib1418@users.noreply.github.com>
2022-08-23 12:20:14 -0500

    Add files via upload
    parallelization created

---
+++ parallelization/deepec.sh
@@ -0,0 +1,45 @@
+#/usr/bin/bash
+
+
+for a in $(seq 1 2 $(cat $1 | wc -l))
+
+do
+b=$(($a+1))
+touch $(cat $1 | head -$a | tail -1).fasta
+echo $(cat $1 | head -$a | tail -1) >> $(echo $(cat $1 | head -$a | tail -1)).fasta
+echo $(cat $1 | head -$b | tail -1) >> $(echo $(cat $1 | head -$a | tail -1)).fasta
+done
+wait
+
+for file in *.fasta;do
+
+python deepec.py -i $file -o ${file}.tsv &
+sleep .5
+done
+
+wait
+
+echo Prediction completed
+sleep 2
+rm deepec_result.txt
+for file in *.tsv;
+do
+cd $file
+cat *.txt | head -2 >> ../deepec_result.txt
+cd ..
+done
+
+wait
+
+for file in *.tsv;do
+echo $file
+rm -r $file
+done
+
+wait
+for file in *.fasta;do
+rm $file
+done
+
+
+echo DONE
